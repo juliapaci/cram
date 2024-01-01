79f4bb0b3eef7f4dbd825b6957c087c3b8640f72
aymey <fabianpaci@gmail.com>
2024-01-01 11:59:25 +1100

    1d tiles

--- SPEC.md
+++ SPEC.md
@@ -4,3 +4,10 @@ heres kinda what i want to achieve:
 - be able to transpile to c and back
 - compile from c to either x86-64 or a custom instruction set
 - also an interpreter and some kind of an intermediate language
+
+# Files
+Cram program files depend on a ruleset defined by the user in a keys image file
+The key file contains the symbols and colours of each token
+an example key image file can be found [here](examples/key.png)
+
+the key file is a 256x256 image read in tiles (64 pixel chunks) from left to right top to bottom in a constant order which is the [key structure](https://github.com/aymey/cram/blob/main/src/processing.rs#L7)
--- src/processing.rs
+++ src/processing.rs
@@ -1,6 +1,5 @@
 use image::io::Reader as ImageReader;
-use image::GenericImageView;
-use image::Rgb;
+use image::{GenericImageView, Rgb};
 use std::collections::HashMap;
 
 // what the colours mean
@@ -15,16 +14,63 @@ struct Key {
     // extras
     string: Rgb<u8>,        // for string literals
     background: Rgb<u8>,    // background colour of the image
+
+    // not a token
+    ignore: Rgb<u8>,
+    grid: Rgb<u8>,
 }
 
 impl Key {
     fn new() -> Self {
         Self {
-            number: Rgb([0, 0, 0]),
+            zero: Rgb([0, 0, 0]),
+            increment: Rgb([0, 0, 0]),
+            decrement: Rgb([0, 0, 0]),
+            access: Rgb([0, 0, 0]),
+            repeat: Rgb([0, 0, 0]),
+
             string: Rgb([0, 0, 0]),
-            background: Rgb([0, 0, 0])
+            background: Rgb([0, 0, 0]),
+
+            ignore: Rgb([0, 0, 0]),
+            grid: Rgb([0, 0, 0]),
         }
     }
+
+    fn identify_background(&mut self, image: &image::DynamicImage) {
+        let mut histogram: HashMap<Rgb<u8>, usize> = HashMap::new();
+        for pixel in image.to_rgb8().pixels() {
+            let counter = histogram.entry(*pixel).or_insert(0);
+            *counter += 1;
+        }
+
+        let background = histogram
+            .iter()
+            .max_by_key(|(_, &count)| count)
+            .unwrap_or((&Rgb([0, 0, 0]), &0));
+
+        self.background = *background.0;
+    }
+
+    // read each 64x64 "tile" and apply the colour inside to the key structure
+    fn read_keys(&mut self, image: &image::DynamicImage) {
+        self.identify_background(image);
+
+        let pixels: Vec<Rgb<u8>> = image.to_rgb8().pixels().map(|&p| p).collect();
+        self.grid = pixels[0];
+
+        let mut tiles: Vec<Rgb<u8>> = pixels
+            .chunks_exact(64)
+            .flat_map(|chunk| chunk.iter())
+            .filter(|&&c| c != self.background && c != self.grid)
+            .cloned()
+            .collect();
+
+        for i in tiles.iter() {
+            println!("{}, {}, {}", i[0], i[1], i[2]);
+        }
+    }
+
 }
 
 struct Lexer {
@@ -40,21 +86,6 @@ impl Lexer {
         }
     }
 
-    pub fn identify_background(image: &image::DynamicImage) -> Rgb<u8> {
-        let mut histogram: HashMap<Rgb<u8>, usize> = HashMap::new();
-        for pixel in image.to_rgb8().pixels() {
-            let mut counter = histogram.entry(*pixel).or_insert(0);
-            *counter += 1;
-        }
-
-        let background = histogram
-            .iter()
-            .max_by_key(|(_, &count)| count)
-            .unwrap_or((&Rgb([0, 0, 0]), &0));
-
-        *background.0
-    }
-
     pub fn lex(image: &image::DynamicImage) {
         let tokens: Vec<u8> = Vec::new();
 
@@ -68,7 +99,7 @@ pub fn deserialize(file_path: &String) -> Result<(), image::ImageError>{
 
     // lex(&image);
     let mut lex = Lexer::new();
-    lex.key.background = Lexer::identify_background(&image);
+    lex.key.read_keys(&image);
     println!("background colour: {}, {}, {}", lex.key.background[0], lex.key.background[1], lex.key.background[2]);
 
     // image.save("output.png")?;
