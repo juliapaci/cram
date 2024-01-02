34177aa5302e8b28bd6622877cf365a4f24c8e69
aymey <fabianpaci@gmail.com>
2024-01-03 08:41:40 +1100

    basic key recognition

--- src/processing.rs
+++ src/processing.rs
@@ -6,14 +6,16 @@ use std::collections::HashMap;
 // what the colours mean
 struct KeyData {
     colour: Rgb<u8>,
-    size: u16
+    width: u16,
+    height: u16
 }
 
 impl KeyData {
     fn new() -> Self {
         Self {
             colour: Rgb([0, 0, 0]),
-            size: 0
+            width: 0,
+            height: 0
         }
     }
 }
@@ -26,9 +28,9 @@ struct Key {
     access: KeyData,        // access a memory address
     repeat: KeyData,        // jump based on a condition
 
-    // language specific syntax
+    // language syntax
     string: KeyData,        // for string literals
-    line_break: KeyData,
+    line_break: KeyData,    // denotes a line seperation of multiple lines on the same row
 
     // not a token
     ignore: Rgb<u8>,        // a colour to ignore
@@ -60,19 +62,17 @@ impl Key {
         [&self.zero, &self.increment, &self.decrement, &self.access, &self.repeat, &self.string, &self.line_break]
     }
 
-    // gets the size of keys that are of the specified colour
-    fn size_from_colour(&self, colour: Rgb<u8>) -> Vec<u16> {
+    // gets the width and height of keys that are of the specified colour
+    fn rect_from_colour(&self, colour: Rgb<u8>) -> Vec<[u16; 2]> {
         self.as_array().iter()
             .filter(|&k| k.colour == colour)
-            .map(|k| k.size)
-            .collect::<Vec<u16>>()
+            .map(|k| [k.width, k.height])
+            .collect::<Vec<[u16; 2]>>()
     }
 
-    // gets the rectangle (width, height) of the keys of the specified colour
-    // fn rect_from_colour(&self, colour:Rgb<u8>) -> Vec<[u16; 2]> {
-    //     self.as_array().iter()
-    //         .filter(|&k| )
-    // }
+    // TODO: make function that gives the left offset (relative to the width of the key) of the first pixel in the tile
+    //       This will optimise searching tiles as it would reduce the amount of pixels that need to be search x2
+    //       This will also fix a bug where multiple keys would be in one keys tile for the lexer
 
     // gets the background colour
     fn identify_background(&mut self, image: &image::DynamicImage) {
@@ -123,20 +123,24 @@ impl Key {
         Ok(())
     }
 
-    // returns the colour of the key
-    // will panic if there is nothing occupying the tile (excluding background and grid)
+    // returns the KeyData (colour, size) of the key in a tile
+    // will panic if there is nothing occupying the tile (or exclusively background and grid pixels)
     fn identify_key_data(&self, tile: &[[Rgb<u8>; 64]; 64]) -> KeyData {
-        let pixels: Vec<&Rgb<u8>> = tile
+        let key: Vec<Vec<&Rgb<u8>>> = tile
             .iter()
-            .flat_map(|row| {
+            .map(|row| {
                 row.iter()
                     .filter(|&p| *p != self.background && *p != self.grid)
+                    .collect()
             })
+            .filter(|row: &Vec<&Rgb<u8>>| !row.is_empty())
             .collect();
 
         KeyData {
-            colour: *pixels[0],
-            size: pixels.len() as u16       // tile cant be more than u16 so converting from usize is safe
+            colour: *key[0][0],
+            // each row is garunteed to exist with data so we can safely unwrap()
+            width: key.iter().map(|row| row.len()).max().unwrap_or(0) as u16,
+            height: key.len() as u16
         }
     }
 
@@ -177,7 +181,7 @@ struct Tile {
 impl Tile {
     fn from_1d(pos: usize, width: u16, height: u16, image: &image::DynamicImage) -> Self {
         Self {
-            x: pos/(image.width() as usize),
+            x: pos%(image.width() as usize),
             y: pos/(image.width() as usize),
             width,
             height
@@ -194,7 +198,7 @@ impl Lexer {
     }
 
     // returns the amount of same coloured pixels in a tile
-    pub fn compute_tile(tile: Tile, image: &image::DynamicImage, colour) -> u32 {
+    pub fn compute_tile(tile: Tile, image: &image::DynamicImage, colour: Rgb<u8>) -> u32 {
         // TODO: to stop computing this everywhere maybe make a getter for it or something
         let pixels: Vec<Rgb<u8>> = image.to_rgb8().pixels().map(|&p| p).collect();
         let mut amount = 0;
@@ -204,7 +208,7 @@ impl Lexer {
         for y in 0..tile.height as usize {
             for x in left..tile.width as isize {
                 let index: isize = (tile.x as isize)+x + ((tile.y + y)*(image.width() as usize)) as isize;
-                if index < 0 {
+                if index < 0 || index > (image.width()*image.height()) as isize {
                     continue;
                 }
 
@@ -223,23 +227,35 @@ impl Lexer {
         // denoting a gap between keys, used to differentiate multiple keys that have the same colour
         let gap: KeyData = KeyData {
             colour: self.key.background,
-            size: 64
+            width: 64,
+            height: 1
         };
 
-        // will keep track of "key" tokens that have been encountered, including their size and colour
+        // will keep track of "key" tokens that have been encountered
         let mut token_buffer: HashMap<Rgb<u8>, usize> = HashMap::new();
         for (i, pixel) in pixels.iter().enumerate() {
             let size = token_buffer.entry(*pixel).or_insert(0);
             *size += 1;
 
             if *pixel == self.key.background || *pixel == self.key.ignore {
+                // TODO: check for gaps
                 continue;
             }
 
-            // if(compute_tile(Tile::from_1d(i, self.key.size_from_colour())))
+            // TODO: dont tile for same pixel for smallest amount pixels of keys with same colour after first one
+            // checking if the amount of pixels in the key is the same as in this tile
+            let rects = self.key.rect_from_colour(*pixel);
+            for rect in rects {
+                if Self::compute_tile(
+                    Tile::from_1d(i, rect[0], rect[1], image),
+                    image, *pixel) == rect[0] as u32 * rect[1] as u32 {
+                    println!("match");
+                }
+            }
         }
     }
 }
+
 pub fn deserialize(key: &String, source: &String) -> Result<(), image::ImageError>{
     let key_img = ImageReader::open(key)?.with_guessed_format()?.decode()?;
     let source_img = ImageReader::open(source)?.with_guessed_format()?.decode()?;
@@ -251,3 +267,5 @@ pub fn deserialize(key: &String, source: &String) -> Result<(), image::ImageErro
 
     Ok(())
 }
+
+// TODO: tests
