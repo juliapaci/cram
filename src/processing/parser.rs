3389ca2c3704c01657677d45317f0e7cb14f9faf
aymey <fabianpaci@gmail.com>
2024-02-15 18:45:12 +1100

    LEXER: current scope in  variants

--- src/processing/parser.rs
+++ src/processing/parser.rs
@@ -28,18 +28,32 @@ struct Parser {
     tokens: VecDeque<Lexeme>
 }
 
+// TODO: not even sure this works
+// TODO: when it works move it to lexer aswell to use it there maybe
+macro_rules! match_lexeme {
+    ($a:expr, $b:pat) => {
+        match $a {
+            Lexeme::Token(($b, _)) => true,
+            _ => false
+        }
+    }
+}
+
 impl Parser {
     // used to evaluate integer literal expressions involving increment and decrement
     fn eval_lit(&mut self) -> isize {
         let mut value = Default::default();
 
         while let Some(lexeme) = self.tokens.pop_front() {
-            if lexeme == Lexeme::Token(Token::LineBreak) {
+            // if let Lexeme::Token((Token::LineBreak, _)) = lexeme {
+            //     break;
+            // }
+            if match_lexeme!(lexeme, Token::LineBreak) {
                 break;
             }
 
-            value += (lexeme == Lexeme::Token(Token::Increment)) as isize;
-            value -= (lexeme == Lexeme::Token(Token::Decrement)) as isize;
+            value += match_lexeme!(lexeme, Token::Increment) as isize;
+            value -= match_lexeme!(lexeme, Token::Decrement) as isize;
         }
 
         value
@@ -48,7 +62,7 @@ impl Parser {
 
     fn parse_expr(&mut self) -> Option<node::Expression> {
         match self.tokens.front() {
-            Some(Lexeme::Token(token)) => {
+            Some(Lexeme::Token((token, _))) => {
                 if *token != Token::Zero  {
                     return None
                 }
@@ -74,20 +88,20 @@ pub fn parse(mut tokens: VecDeque<Lexeme>) -> Result<node::Program, String> {
 
     while let Some(lexeme) = parser.tokens.pop_front() {
             match lexeme {
-                Lexeme::Token(Token::Zero) => {}
-                Lexeme::Token(Token::Increment) => {}
-                Lexeme::Token(Token::Decrement) => {}
-                Lexeme::Token(Token::Access) => {}
-                Lexeme::Token(Token::Repeat) => {}
-                Lexeme::Token(Token::Quote) => {}
-                Lexeme::Token(Token::LineBreak) => {}
+                Lexeme::Token((Token::Zero, _))         => {}
+                Lexeme::Token((Token::Increment, _))    => {}
+                Lexeme::Token((Token::Decrement, _))    => {}
+                Lexeme::Token((Token::Access, _))       => {}
+                Lexeme::Token((Token::Repeat, _))       => {}
+                Lexeme::Token((Token::Quote, _))        => {}
+                Lexeme::Token((Token::LineBreak, _))    => {}
 
-                Lexeme::Token(Token::ScopeStart) => {}
-                Lexeme::Token(Token::ScopeEnd) => {}
+                Lexeme::Token((Token::ScopeStart, _))   => {}
+                Lexeme::Token((Token::ScopeEnd, _))     => {}
 
-                Lexeme::Identifier(id) => {}
+                Lexeme::Identifier((id, _)) => {}
 
-                Lexeme::Token(Token::Variable) => {} // not possible
+                Lexeme::Token((Token::Variable, _)) => {} // not possible
             }
     }
 
